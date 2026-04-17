245210f96444584d6e63c4a5058189fba03c7896
Sérvulo Pires da Rocha Júnior <servulo.ia@hotmail.com>
2026-04-17 14:16:57 -0300

    acerto no endereço do backend

--- todo-frontend/src/environments/environment.prod.ts
+++ todo-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sprj-ca-todo-backend.salmonhill-9ed0043a.brazilsouth.azurecontainerapps.io'
+  apiUrl: 'https://todo-api.servulo.dev.br'
 };
